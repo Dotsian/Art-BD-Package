1c75fec6b8f2f3bd4fd9219c405f13846ba407f6
Dotsian <selbydarryl05@gmail.com>
2025-04-08 12:58:46 -0400

    WIP `acceptart` command

--- art/cog.py
+++ art/cog.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
-from typing import TYPE_CHECKING
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Any
 
 import discord
 from discord.ext import commands
@@ -13,6 +14,23 @@ if TYPE_CHECKING:
 STATIC = os.path.isdir("static")
 FILE_PREFIX = "." if STATIC else ""
 
+@dataclass
+class MessageLink:
+    bot: Any
+
+    guild: discord.Guild | None = None
+    thread: discord.Thread | None = None
+    message: discord.Message | None = None
+
+    async def from_link(self, link: str):
+        parsed_link = link.split("/")
+
+        self.guild = self.bot.get_guild(int(parsed_link[4]))
+        self.thread = self.guild.get_thread(int(parsed_link[5]))
+        self.message = await thread.fetch_message(int(parsed_link[6]))
+
+        return self.message
+
 class ArtCog(commands.Cog):
     def __init__(self, bot: "BallsDexBot"):
         self.bot = bot
@@ -22,7 +40,7 @@ class ArtCog(commands.Cog):
     async def updateart(self, ctx: commands.Context, channel: discord.ForumChannel):
         """
         If a countryball's spawn art is outdated in the specified forum channel, 
-        they will automatically be updated to match the current spawn artwork.
+        it will automatically be updated to match the current spawn artwork.
 
         Parameters
         ----------
@@ -40,9 +58,13 @@ class ArtCog(commands.Cog):
             thread_message = await thread.fetch_message(thread.id)
 
             thread_artwork_path = thread_message.attachments[0].filename
-            ball_artwork_path = await Ball.get(country=thread.name).values_list(
+            ball_artwork_path = await Ball.get_or_none(country=thread.name).values_list(
                 "wild_card", flat=True
             )
+
+            if ball_artwork_path is None:
+                await ctx.send(f"Could not find {thread.name}")
+                continue
 
             prefix = "/static/uploads/" if STATIC else ""
 
@@ -77,15 +99,17 @@ class ArtCog(commands.Cog):
             The channel you want to generate the spawn art in.
         """
         threads_created = 0
-        threads = [x.name for x in channel.threads]
+        existing_threads = {x.name for x in channel.threads}
 
         await ctx.send(
             "Generating threads...\n"
             "-# This may take a while depending on the amount of collectibles you have."
         )
 
-        for ball in await Ball.filter(enabled=True):
-            if ball.country in threads:
+        balls = await Ball.filter(enabled=True)
+
+        for ball in balls:
+            if ball.country in existing_threads:
                 continue
 
             try:
@@ -100,6 +124,35 @@ class ArtCog(commands.Cog):
 
             threads_created += 1
 
-            await asyncio.sleep(0.75)
-
         await ctx.send(f"Created `{threads_created}` threads")
+
+    async def acceptart(
+        self, ctx: commands.Context, message_link: str
+    ):
+        """
+        Accepts spawn art in a thread.
+
+        Parameters
+        ----------
+        channel: discord.ForumChannel
+            The channel that contains the art.
+        message_link: str
+            The messsage link containing the art.
+        """
+        message_link = MessageLink(self.bot)
+
+        try:
+            message = await message_link.from_link(message_link)
+        except Exception as error:
+            await ctx.send(
+                f"An error occured while trying to retrieve the message.\n```{error}```"
+            )
+            return
+
+        ball = await Ball.get_or_none(country=thread.name)
+
+        if ball is None:
+            await ctx.send(f"{ball.country} doesn't exist.")
+            return
+
+        await message.add_reaction("✅")
